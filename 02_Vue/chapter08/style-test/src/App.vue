97e429975ad59f3e3ecdd9b7d33eb99ae94a22b2
data0gyus <dwjsk2008@gmail.com>
2025-03-26 13:09:40 +0900

    20250326 수업 자료

---
+++ 02_Vue/chapter08/style-test/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Child1 />
+    <Child3 />
+    <Child2 />
+  </div>
+</template>
+<script>
+import Child1 from "./components/Child1.vue";
+import Child3 from "./components/Child3.vue";
+import Child2 from "./components/Child2.vue";
+
+export default {
+  name: "App",
+  components: { Child1, Child2, Child3 },
+};
+</script>
---
+++ 02_Vue/chapter08/style-test/src/components/checkboxs/CheckBoxList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-for="item in items" :key="item.id">
+    <slot name="icon" :checked="item.checked"></slot>
+    <!-- {checked}-->
+    <input
+      type="checkbox"
+      :value="item.id"
+      :checked="item.checked"
+      @change="
+        $emit('check-changed', { id: item.id, checked: $event.target.checked })
+      "
+    />
+    <slot :checked="item.checked" :label="item.label"></slot>
+    <!-- {checked, label }-->
+  </div>
+</template>
+<script>
+export default {
+  name: "CheckBoxList",
+  props: ["items"], // 배열
+};
+</script>

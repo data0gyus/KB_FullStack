1af30423ed2c7e4f07a5364cdb95d9d48833f119
data0gyus <dwjsk2008@gmail.com>
2025-03-26 14:50:45 +0900

    20250326 오후 Summary

--- 02_Vue/chapter08/style-test/src/main.js
+++ 02_Vue/chapter08/style-test/src/main.js
@@ -1,10 +1,8 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 // import App from "./App.vue";
 // import App from "./App1.vue";
 // import App from "./App2.vue";
-// import App from "./App3.vue";
-import App from "./App4.vue";
+import App from "./App3.vue";
+// import App from "./App4.vue";
 
 createApp(App).mount("#app");

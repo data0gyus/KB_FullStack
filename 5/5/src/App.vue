a5ede810d6f4e415df9aaa01c3c1c2e1c389af7e
data0gyus <dwjsk2008@gmail.com>
2025-04-01 16:32:13 +0900

    실습 예제

---
+++ 5/5/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <CommonHeader />
+  <MainGallary />
+</template>
+
+<script setup>
+import CommonHeader from './components/CommonHeader.vue'
+import MainGallary from './components/MainGallary.vue'
+</script>

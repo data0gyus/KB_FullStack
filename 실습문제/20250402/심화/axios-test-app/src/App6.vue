2ce5f3b52d09217c5956ab0a9c89e74186df6438
data0gyus <dwjsk2008@gmail.com>
2025-04-03 09:20:43 +0900

    20250402 심화 문제

--- 실습문제/20250402/심화/axios-test-app/src/App6.vue
+++ 실습문제/20250402/심화/axios-test-app/src/App6.vue
@@ -1,18 +1,19 @@
 <script setup>
 import axios from "axios";
+
 const requestAPI = async () => {
   const url = "/api/todos2";
   axios
     .get(url, { timeout: 900 })
     .then((response) => {
-      console.log("#응답객체 : ", response);
+      console.log("# 응답객체:", response);
     })
     .catch((e) => {
-      console.log("에러 =================");
+      console.log("에러==================");
       console.log(e);
       if (e instanceof Error) console.log(e.message);
       else console.log(e);
     });
 };
-requestAPI;
+requestAPI();
 </script>

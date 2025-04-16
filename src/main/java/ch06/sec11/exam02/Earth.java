d5513c577d5ada62668018ff824db9e0df6c3ea3
data0gyus <dwjsk2008@gmail.com>
2025-04-16 17:20:11 +0900

    20250415 기초 풀이 및 20250416 과제 제출 완료

---
+++ src/main/java/ch06/sec11/exam02/Earth.java
@@ -0,0 +1,12 @@
+package ch06.sec11.exam02;
+
+public class Earth {
+    static final double RADIUS = 64000;
+
+    static final double EARTH_SURFACE_AREA;
+
+    static {
+        EARTH_SURFACE_AREA = 4 * Math.PI * RADIUS * RADIUS;
+    }
+
+}

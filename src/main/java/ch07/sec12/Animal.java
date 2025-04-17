a50ec8e8f4886f592c804aedc7d2d0da8774f674
data0gyus <dwjsk2008@gmail.com>
2025-04-17 15:14:35 +0900

    2025-04-17 오후 수업까지

---
+++ src/main/java/ch07/sec12/Animal.java
@@ -0,0 +1,10 @@
+package ch07.sec12;
+
+public class Animal {
+
+    protected String name;
+
+    public void sound() {
+        System.out.println("Animal Sound");
+    }
+}
---
+++ src/main/java/ch07/sec12/Dog.java
@@ -0,0 +1,15 @@
+package ch07.sec12;
+
+public class Dog extends Animal {
+
+
+    public Dog(){
+        super.name = "초코";
+    }
+
+    @Override
+    public void sound() {
+        System.out.println(name + " : 왈왈, 멍멍, 깨갱");
+    }
+}
+

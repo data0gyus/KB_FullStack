68b8153499c18952317db955cfd9ae86c3df5811
data0gyus <dwjsk2008@gmail.com>
2025-04-17 16:12:38 +0900

    2025-04-17 심화 문제

---
+++ src/main/java/practice0417/Adbanced/ch07/exam/HttpServletExample.java
@@ -0,0 +1,11 @@
+package practice0417.Adbanced.ch07.exam;
+
+public class HttpServletExample {
+    public static void main(String[] args) {
+        method(new LoginServlet());
+        method(new FileDownloadServlet());
+    }
+    public static void method(HttpServlet servlet) {
+        servlet.service();
+    }
+}

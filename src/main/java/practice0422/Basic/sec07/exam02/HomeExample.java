46751f958a5bc0f08d4fbd07aa98caa73e46d192
data0gyus <dwjsk2008@gmail.com>
2025-04-22 14:46:59 +0900

    2025-04-22 기본 문제

---
+++ src/main/java/practice0422/Basic/sec07/exam02/HomeExample.java
@@ -0,0 +1,8 @@
+package practice0422.Basic.sec07.exam02;
+
+public class HomeExample {
+    public static void main(String[] args) {
+        Home h = new Home();
+        h.use1();
+    }
+}
---
+++ src/main/java/practice0422/Basic/sec07/exam02/RemoteControl.java
@@ -0,0 +1,7 @@
+package practice0422.Basic.sec07.exam02;
+
+public interface RemoteControl {
+    // 추상 메서드
+    void turnOn();
+    void turnOff();
+}

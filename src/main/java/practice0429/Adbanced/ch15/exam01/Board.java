f54fc035ae91d167897483537f3e358f979531cd
data0gyus <dwjsk2008@gmail.com>
2025-04-29 15:43:31 +0900

    2025-04-29 심화 문제

---
+++ src/main/java/practice0429/Adbanced/ch15/exam01/Board.java
@@ -0,0 +1,14 @@
+package practice0429.Adbanced.ch15.exam01;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Board {
+    private String subject;
+    private String content;
+    private String writer;
+}
